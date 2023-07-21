e55103e770ebbdc25b992ab9c526ce2633ec9543
Itodo Emmanuel Itodo <54606115+itodoemmanuel@users.noreply.github.com>
2023-07-21 14:53:27 +0100

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,43 +1,37 @@
 #!/usr/bin/python3
+'''a script that reads stdin line by line and computes metrics'''
+
+
 import sys
 
-def parse_line(line):
-    try:
-        _, _, _, status_code, file_size = line.split()[0:5]
-        status_code = int(status_code)
-        file_size = int(file_size)
-        return status_code, file_size
-    except (ValueError, IndexError):
-        return None, None
+cache = {'200': 0, '301': 0, '400': 0, '401': 0,
+         '403': 0, '404': 0, '405': 0, '500': 0}
+total_size = 0
+counter = 0
 
-def print_statistics(total_file_size, status_counts):
-    print(f"Total file size: {total_file_size}")
-    for status_code in sorted(status_counts):
-        print(f"{status_code}: {status_counts[status_code]}")
+try:
+    for line in sys.stdin:
+        line_list = line.split(" ")
+        if len(line_list) > 4:
+            code = line_list[-2]
+            size = int(line_list[-1])
+            if code in cache.keys():
+                cache[code] += 1
+            total_size += size
+            counter += 1
 
-def main():
-    total_file_size = 0
-    status_counts = {}
+        if counter == 10:
+            counter = 0
+            print('File size: {}'.format(total_size))
+            for key, value in sorted(cache.items()):
+                if value != 0:
+                    print('{}: {}'.format(key, value))
 
-    try:
-        line_count = 0
-        for line in sys.stdin:
-            line = line.strip()
-            status_code, file_size = parse_line(line)
-            if status_code is None:
-                continue
+except Exception as err:
+    pass
 
-            total_file_size += file_size
-            status_counts[status_code] = status_counts.get(status_code, 0) + 1
-
-            line_count += 1
-            if line_count % 10 == 0:
-                print_statistics(total_file_size, status_counts)
-
-    except KeyboardInterrupt:
-        pass
-
-    print_statistics(total_file_size, status_counts)
-
-if __name__ == "__main__":
-    main()
+finally:
+    print('File size: {}'.format(total_size))
+    for key, value in sorted(cache.items()):
+        if value != 0:
+            print('{}: {}'.format(key, value))
